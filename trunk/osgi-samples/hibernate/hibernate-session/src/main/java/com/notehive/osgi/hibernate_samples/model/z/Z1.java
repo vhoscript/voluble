6b1f03737689bd2e444071e47892107baa880117
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2008-04-15 20:31:44 +0000

    backup checkin.  basic hsql test case runs.

---
+++ trunk/osgi-samples/hibernate/hibernate-session/src/main/java/com/notehive/osgi/hibernate_samples/model/z/Z1.java
@@ -0,0 +1,48 @@
+package com.notehive.osgi.hibernate_samples.model.z;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Z1")
+public class Z1 {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "string1")
+	private String string1;
+
+	@Column(name = "string2")
+	private String string2;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getString1() {
+		return string1;
+	}
+
+	public void setString1(String string1) {
+		this.string1 = string1;
+	}
+
+	public String getString2() {
+		return string2;
+	}
+
+	public void setString2(String string2) {
+		this.string2 = string2;
+	}
+
+}
