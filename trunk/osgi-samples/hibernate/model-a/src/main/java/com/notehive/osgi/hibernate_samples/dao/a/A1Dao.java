0d5621eed594ded23d9c40ad4eacf74032fcefd8
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2008-04-16 21:45:49 +0000

    Adding model-a.  Not in integration tests yet.

---
+++ trunk/osgi-samples/hibernate/model-a/src/main/java/com/notehive/osgi/hibernate_samples/dao/a/A1Dao.java
@@ -0,0 +1,39 @@
+package com.notehive.osgi.hibernate_samples.dao.a;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.notehive.osgi.hibernate_samples.model.a.A1;
+
+public class A1Dao extends HibernateDaoSupport {
+
+	public void save(A1 z1) {
+		getHibernateTemplate().save(z1);
+	}
+
+	/**
+	 * 'load' returns a proxy with only the id populated.
+	 * To access members in the proxy requires a Hibernate
+	 * session.  Haven't figured out how to do transactions
+	 * between bundles yet... 
+	 */
+	public A1 load(long id) {
+		return (A1) getHibernateTemplate().load(A1.class, id);
+	}
+
+	public A1 get(long id) {
+		return (A1) getHibernateTemplate().get(A1.class, id);
+	}
+
+	public void update(A1 z1) {
+		getHibernateTemplate().update(z1);
+	}
+
+	public void delete(A1 z1) {
+		getHibernateTemplate().delete(z1);
+	}
+
+}
