f7e34089b43ef2ef3a5a6ccf336c9c3d67899966
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2008-05-16 18:00:54 +0000

    Added model-b to integration test and deployment projects.

--- trunk/osgi-samples/hibernate/integration-tests/src/test/java/com/notehive/osgi/hibernate_samples/integration/BundleCreatorTest.java
+++ trunk/osgi-samples/hibernate/integration-tests/src/test/java/com/notehive/osgi/hibernate_samples/integration/BundleCreatorTest.java
@@ -23,7 +23,8 @@ public class BundleCreatorTest extends AbstractConfigurableBundleCreatorTests {
 				"com.notehive.osgi.hibernate-samples, hsqldb, 1.8.0.7",
 				"com.notehive.osgi.hibernate-samples, hibernate-classes, 3.2.6.ga",
 				"com.notehive.osgi.hibernate-samples, hibernate-session, 1.0.0.SNAPSHOT",
-				"com.notehive.osgi.hibernate-samples, model-a, 1.0.0.SNAPSHOT" 
+				"com.notehive.osgi.hibernate-samples, model-a, 1.0.0.SNAPSHOT",
+				"com.notehive.osgi.hibernate-samples, model-b, 1.0.0.SNAPSHOT" 
 				};
 	}
 
