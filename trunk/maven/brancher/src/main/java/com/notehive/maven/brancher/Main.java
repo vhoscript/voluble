2c5a1fe0841f274e497f9f90af0d2617558b05f2
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2008-09-05 17:38:49 +0000

    dump dependencies into target directory

--- trunk/maven/brancher/src/main/java/com/notehive/maven/brancher/Main.java
+++ trunk/maven/brancher/src/main/java/com/notehive/maven/brancher/Main.java
@@ -3,7 +3,14 @@ package com.notehive.maven.brancher;
 public class Main {
 	
 	public static void main(String[] args) {
-		throw new RuntimeException("not implemented");
+		MakeSnapshot makeSnapshot = new MakeSnapshot();
+		
+		makeSnapshot.setRootFolder("/home/loedolff/dev/core-3.3.1/common/util");
+		makeSnapshot.setFromVersion("3.3.0");
+		makeSnapshot.setToVersion("3.3.1-SNAPSHOT");
+		makeSnapshot.setScmFrom("tags/releases/3.3.0");
+		makeSnapshot.setScmTo("branches/3.3.1");
+		makeSnapshot.switchVersion();
 	}
 
 }
