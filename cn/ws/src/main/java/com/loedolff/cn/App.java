de3210155b4d9fdd0dec37d4d861c8e399b1f04d
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2010-10-08 23:40:11 +0000

    tweet!

--- cn/ws/src/main/java/com/loedolff/cn/App.java
+++ cn/ws/src/main/java/com/loedolff/cn/App.java
@@ -1,13 +1,34 @@
 package com.loedolff.cn;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+
+import org.xml.sax.SAXException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, SAXException
     {
         System.out.println( "Hello World!" );
+    	System.out.println( new Date().toString() );
+
+    	CharacterHistogram wh1 = new CharacterHistogram();
+    	wh1.addWords(new URL("http://cn.wsj.com"));
+    	
+//    	System.out.println();
+//    	System.out.println("Combined");
+//    	System.out.println("--------");
+//    	wh1.addWords(new URL("file:src/test/resources/wsj2.html"));
+    	
+    	wh1.writeHistogram("target/histogram.txt");
+    	wh1.writeHtmlHistogram("target/histogram.html");
+    	
+    	Recommendations r = new Recommendations();
+    	r.recommend(wh1);
     }
 }
