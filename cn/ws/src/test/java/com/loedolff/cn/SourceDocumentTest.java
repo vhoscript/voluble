b027cec1bd90e2518e90780e46c746992c8d41ed
Hans.Loedolff <Hans.Loedolff@06d4312c-dd4a-0410-a1ab-23394b46d89d>
2010-10-09 06:51:32 +0000

    Adding SourceDocument.

---
+++ cn/ws/src/test/java/com/loedolff/cn/SourceDocumentTest.java
@@ -0,0 +1,16 @@
+package com.loedolff.cn;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+public class SourceDocumentTest {
+
+	
+	@Test public void testRead() throws IOException, SAXException {
+		SourceDocument sd = new SourceDocument(new URL("http://cn.wsj.com"));
+		sd.write("target/wsj.html");
+	}
+}
